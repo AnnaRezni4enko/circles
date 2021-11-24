a5dfb934b86fb7473a87a17ff75ad13f0999b0b1
AnnRezni4enko <AnnRezni4enko@yandex.ru>
2021-11-24 22:47:53 +0300

    colorful circles

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
-from PyQt5 import uic
 import sys
 from PyQt5.QtCore import QPoint
 from random import randint
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-class MyWidget(QMainWindow):
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(602, 603)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.btn = QtWidgets.QPushButton(self.centralwidget)
+        self.btn.setGeometry(QtCore.QRect(220, 20, 131, 28))
+        self.btn.setObjectName("btn")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 602, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.btn.setText(_translate("MainWindow", "Нажми на меня"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.initUI()
 
     def initUI(self):
@@ -28,23 +56,19 @@ class MyWidget(QMainWindow):
         self.repaint()
 
     def draw(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
-        r = randint(5, 70)
-        qp.drawEllipse(QPoint(150, 200), r, r)
-        r = randint(5, 70)
-        qp.drawEllipse(QPoint(300, 200), r, r)
-        r = randint(5, 70)
-        qp.drawEllipse(QPoint(450, 200), r, r)
-        r = randint(5, 70)
-        qp.drawEllipse(QPoint(150, 400), r, r)
-        r = randint(5, 70)
-        qp.drawEllipse(QPoint(300, 400), r, r)
-        r = randint(5, 70)
-        qp.drawEllipse(QPoint(450, 400), r, r)
+        for x in range(150, 451, 150):
+            for y in range(200, 401, 200):
+                r = randint(0, 255)
+                g = randint(0, 255)
+                b = randint(0, 255)
+                qp.setBrush(QColor(r, g, b))
+                radius = randint(5, 70)
+                qp.drawEllipse(QPoint(x, y), radius, radius)
 
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
